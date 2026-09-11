2e925fc1499bbd9b0a0c6da98b1f1cf6c9def728
hafshahluthfiah <hafshahluthfiah@gmail.com>
2026-09-11 14:54:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = Object.freeze({
   // Tempel URL deployment Apps Script yang berakhiran /exec di bawah ini.
-  appScriptUrl: 'https://7vp2adu.s.gy/pengeluaran'
+  appScriptUrl: 'https://script.google.com/macros/s/AKfycbz6eq4PqOYbcLZybg22xGHxo_enWUaPnQJE6WOexNlLlL87of1QdMQ20vW0HKbZCp-w/exec'
 });
